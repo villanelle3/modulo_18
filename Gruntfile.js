626f6ac0b1c48b046a33bc4efba6022ebf2d091c
Laura Augusta <laurah.algusta@gmail.com>
2023-02-11 01:10:49 -0300

    updating my branchs

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,74 @@ module.exports = function(grunt){
         less: {
             development: {
                 files: {
-                'main.css': 'main.less'
+                './build/styles/main.css': './src/styles/main.less'
                 }
             },
-        }
-    });
+            production: {
+                options: {
+                    compress: true,
+                }, 
+                files: {
+                    './dist/styles/main.min.css': './src/styles/main.less'
+                }
+            }
+        },
+        watch:{
+            less: {
+                files: ["./src/styles/**/*.less"],
+                tasks: ['less:development']
+                    
+            }
+        },
+        replace:{
+            dev: {
+                options: {
+                    patterns: [{
+                        match: 'ENDEREÇO_DO_CSS',
+                        replacement: './styles/main.css'
+                    }]
+                },
+                files: [
+                    {
+                        expand: true, flatten: true, src: ['src/index.html'], dest: 'build/'
+                    }
+                ]
+            },
+
+            dist: {
+                options: {
+                    patterns: [{
+                        match: 'ENDEREÇO_DO_CSS',
+                        replacement: './styles/main.min.css'
+                    }]
+                },
+                files: [
+                    {
+                        expand: true, flatten: true, src: ['prebuild/index.html'], dest: 'dist/'
+                    }
+                ]
+            },
+            
+
+        },
+        htmlmin: {                                     
+            dist: {                                      
+                options: {                             
+                removeComments: true,
+                collapseWhitespace: true
+                },
+            files: {                                   
+                'prebuild/index.html':'src/index.html',     
+                }
+            },
+        }           
+    }); 
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    grunt.registerTask('default', ['less',]);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', "htmlmin:dist", "replace:dist"]);
 }
